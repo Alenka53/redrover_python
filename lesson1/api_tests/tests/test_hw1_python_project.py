885a113d62f1832be98082e8e92b7c12e936abce
Alyona <alyonashimanskaya528@gmail.com>
2024-11-08 17:49:05 +0200

    hw3,part1

--- lesson1/api_tests/tests/test_hw1_python_project.py
+++ lesson1/api_tests/tests/test_hw1_python_project.py
@@ -3,6 +3,7 @@ import pytest
 from lesson1.api_tests.case.pom.case import create_case
 from lesson1.api_tests.case.models.case import Case, Case1
 from lesson1.api_tests.case.data.case import create_case_dict, create_case_dict1
+from lesson1.api_tests.utils.api_client import client
 
 
 @pytest.mark.xfail(reason="Not enough data (priority is lost)")
@@ -11,4 +12,12 @@ def test_create_case1():
     response.status_code_should_be_eq(200)
     response.json_should_be_eq(Case1(**create_case_dict1).model_dump())
     response.schema_should_be_eq(Case1(**create_case_dict1).model_json_schema())
-
+  
+def test_delete_case():
+    response = client.make_request(
+        handle="/testcases",
+        method = "DEL",
+        json = {"test_case_id":1}
+    )
+    response.status_code_should_be_eq(200)
+    response.json_should_be_eq({"detail": "Test case deleted."})
